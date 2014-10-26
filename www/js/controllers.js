880ed9b99be6fde55abd80669a522a10c16ee2c7
cgspicer <cgspicer@gmail.com>
2014-10-26 17:13:01 -0400

    refactoring some of the stream code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,57 +1,110 @@
 angular.module('wras-player.controllers', [])
+// stream of album 88 factory
+.factory( 'stream88', [ '$rootScope', '$q', '$http', '$ionicLoading', function( $rootScope, $q, $http, $ionicLoading ) {
 
-.controller('PlayerCtrl', function($scope, $http, $ionicLoading) {
-  $ionicLoading.show({
-    template: 'Loading...'
-  });
   var stream;
-  var disabledStyle = {'background-color': '#444'};
-  var activeStyle = {'background-color': '#262626'};
-  var checkM3uPlaylist = function() {
-    $http.get('http://www.publicbroadcasting.net/wras/ppr/wras2.m3u')
-    .success(function (data, status, headers, config) {
-      var lines = data.split("\n");
-      for ( var i=0;i<lines.length;i++){
-        if ( lines[i].indexOf('http://') > -1 ) {
-          var streamUrl = lines[i];
-          stream = new Audio();
-          stream.src = streamUrl;
-          stream.load();
-          $scope.playStyle = activeStyle;
-          $scope.pauseStyle = disabledStyle;
-          $scope.isPlaying = true;
-          setTimeout( function(){
-              $ionicLoading.hide();
-              $scope.showControls = true;
-            }, 1500);
-        }
-      }
-
-    }).error(function (data, status, headers, config) {
-      // console.log(status);
+  // defer a call to the stream information
+  var loadStream = function(){
+    var streamDeferred = $q.defer();
+    $ionicLoading.show({
+      template: 'Loading Stream...'
     });
-  }
+    $http.get('http://www.publicbroadcasting.net/wras/ppr/wras2.m3u')
+    .success(function ( data, status ) {
+      streamDeferred.resolve( data );
+    }).error(function ( data, status ) {
+      streamDeferred.resolve( data );
+    });
+    var handleStream = streamDeferred.promise;
+    // handle the resolved stream information
+    handleStream.then( function( data, status ) {
+      var streamUrl = parseStreamURL( data );
+      stream = new Audio();
+      stream.addEventListener( 'canplay', handleLoad );
+      stream.src = streamUrl;
+      stream.load();
+      // set up our event listeners
+    });
+  };
+
+  var pauseStream = function() {
+    stream.pause();
+    $rootScope.$broadcast('streamPaused');
+  };
+  var playStream = function() {
+    stream.play();
+    $rootScope.$broadcast('streamPlaying');
+  };
+  var refreshStream = function() {
+    $ionicLoading.show({
+      template: 'Loading Stream...'
+    });
+    stream.pause();
+    stream = null;
+    loadStream();
+    $rootScope.$broadcast('streamPaused');
+  };
+  // handlers for stream events
+  var handleLoad = function() {
+    $rootScope.showControls = true;
+    $ionicLoading.hide();
+    playStream();
+  };
+
+  var parseStreamURL = function( msg ) {
+    var lines = msg.split("\n");
+    for ( var i=0;i<lines.length;i++){
+      if ( lines[i].indexOf('http://') > -1 ) {
+        var streamUrl = lines[i];
+        return streamUrl;
+      }
+    }
+  };
+
+  // INIT
+  $rootScope.showControls = false;
+  (function(){
+    loadStream();
+  })();
+
+  // return interface
+  return {
+    play          : playStream,
+    pause         : pauseStream,
+    refresh       : refreshStream
+  };
+
+}])
+// Player view controller
+.controller('PlayerCtrl', [ '$scope', 'stream88', function( $scope, stream88 ) {
+
+  // INIT
+  var disabledStyle = {'background-color': '#444'};
+  var activeStyle   = {'background-color': '#262626'};
+
+  $scope.playStream = function() {
+    if ( $scope.isPlaying ) return;
+    stream88.play();
+  };
   $scope.pauseStream = function() {
-    try { stream.pause(); } catch(err) { }
+    if ( !$scope.isPlaying ) return;
+    stream88.pause();
+  };
+  $scope.refreshStream = function() {
+    stream88.refresh();
+  };
+
+  $scope.$on( 'streamPaused', function() {
     $scope.isPlaying = false;
     $scope.playStyle = disabledStyle;
     $scope.pauseStyle = activeStyle;
-  }
-  $scope.playStream = function() {
-    try { stream.play(); } catch(err) { }
-    $scope.isPlaying = true;
-    $scope.playStyle = activeStyle;
+  });
+
+  $scope.$on( 'streamPlaying', function() {
+    $scope.isPlaying  = true;
+    $scope.playStyle  = activeStyle;
     $scope.pauseStyle = disabledStyle;
-  }
-  $scope.refreshStream = function() {
-    $scope.playStyle = disabledStyle;
-    $scope.pauseStyle = disabledStyle;
-    try { stream.pause(); } catch(err) { }
-    $scope.isPlaying = false;
-    $ionicLoading.show({
-      template: 'Loading...'
-    });
-    checkM3uPlaylist();
-  }
-  $scope.refreshStream();
-});
+  });
+
+}])
+;
